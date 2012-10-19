bc23ee6e45299ee5a72a8eb5879652070d61e83b
Torossian <fabtor91@gmail.com>
2012-10-19 16:00:34 +0200

    Test2
    Go

--- src/Exo1.java
+++ src/Exo1.java
@@ -7,6 +7,7 @@ public class Exo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ( "Test GIT" );
+		System.out.println ( "Test GIT 2" );
 	}
 
 }
